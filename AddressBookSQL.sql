a18a9c8d6c6a1fde961d458b732751a20116c9de
durgachandrasekhar <durgachandrasekhar3@gmail.com>
2021-09-08 13:25:51 +0530

    Address book type

--- AddressBookSQL.sql
+++ AddressBookSQL.sql
@@ -41,3 +41,11 @@ select COUNT(firstname) from AddressBook where city = 'Memphis' or state = 'Tenn
 
 
 select * from AddressBook where city = 'Memphis' order by firstname
+
+ALTER TABLE AddressBook 
+ADD type varchar(150),
+	name varchar(100)
+update AddressBook set type = 'Friend', name = 'FriendsBook' where firstname in ('Matthew','David','Tyler','Janice','Liam');
+update AddressBook set type = 'Family', name = 'FamilyBook' where firstname in ('Ismael','Byron');
+update AddressBook set type = 'Colleague', name = 'Office' where firstname = 'James'
+select * from AddressBook
